266ef5b43b55a123aae93a5cab9e73e1d33762b2
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-01-28 13:38:54 +0600

    knight moves

--- chessengine/ChessEngine.py
+++ chessengine/ChessEngine.py
@@ -131,10 +131,30 @@ class GameState:
                 else:
                     break
 
-    def getQueenMoves(self, r, c, moves):
-        pass
-
     def getKnightMoves(self, r, c, moves):
+        knightMoves = (
+            (-2, -1),
+            (-2, 1),
+            (-1, -2),
+            (-1, 2),
+            (1, -2),
+            (1, 2),
+            (2, -1),
+            (2, 1),
+        )
+
+        allyColor = "w" if self.whiteToMove else "b"
+
+        for m in knightMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
+
+    def getQueenMoves(self, r, c, moves):
         pass
 
     def getKingMoves(self, r, c, moves):
